2f7e084863fd92ff37e7ab72fff0970ce23984dc
Si Hyeong Lee <super2451894@naver.com>
2017-08-24 00:01:28 +0900

    variables

---
+++ JavaScript/Variable/Declaring Variables.js
@@ -0,0 +1,13 @@
+var a = 1;
+console.log(a + 1); //2
+
+var a = 2;
+console.log(a + 1); //3
+
+var first = "coding";
+console.log(first + " everybody");
+
+var a = 'coding',
+  b = 'everybody';
+console.log(a);
+console.log(b);
