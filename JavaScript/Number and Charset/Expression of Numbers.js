a43fa157ccfb777ca595865ab9174c69de6bf05c
Si Hyeong Lee <super2451894@naver.com>
2017-08-23 21:59:28 +0900

    Expression of Numbers.js
    upload

---
+++ JavaScript/Number and Charset/Expression of Numbers.js
@@ -0,0 +1,14 @@
+alert(1 + 1); //2
+
+alert(1.2 + 1.3); //2.5
+
+alert(2 * 5); //10
+
+alert(6 / 2); //3
+
+Math.pow(3, 2); //9 3^2
+Math.round(10, 6); //11 Rounding of 10.6
+Math.ceil(10.2); //11 Round up of 10.2
+Math.floor(10.6); //10 Round down of 10.6
+Math.sqrt(9); //3 Square root of 3
+Math.random(); // A random number between 0 and 1.0
