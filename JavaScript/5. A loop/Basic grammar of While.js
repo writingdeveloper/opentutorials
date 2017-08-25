5d9c4256350268e1f5180f04f4577dcbc943fe6c
Si Hyeong Lee <super2451894@naver.com>
2017-08-25 15:52:59 +0900

    Basic grammer of While.js Upload

---
+++ JavaScript/5. A loop/Basic grammar of While.js
@@ -0,0 +1,14 @@
+/*
+
+Document.write uses JavaScript to output text to a web page. This will only work in a web browser. If you are practicing in the same environment as the node.js console, use a method such as console.log instead.
+
+*/
+
+
+while (true) {
+  console.log('coding everybody'); //Print coding everybody
+}
+
+while (false) {
+  console.log('This will not appear'); //Print Nothing
+}
