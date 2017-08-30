2f334e56c3115a0c583f1f65b8e3dd803353f7bc
Si Hyeong Lee <super2451894@naver.com>
2017-08-30 23:17:28 +0900

    Eliminate the Elements of array.js

---
+++ JavaScript/7. Array/Eliminate the elements of the array.js
@@ -0,0 +1,23 @@
+var li=['a','b','c','d','e'];
+li.shift();
+console.log(li);
+
+// bcde
+
+var li2=['a','b','c','d','e'];
+li2.pop();
+console.log(li2);
+
+//abcd
+
+var li3=['c','b','a','d','e'];
+li3.sort();
+console.log(li3);
+
+// abcde
+
+var li4=['c','a','b','d','e'];
+li4.reverse();
+console.log(li4);
+
+// edbac
