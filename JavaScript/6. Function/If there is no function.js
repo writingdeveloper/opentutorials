9fb49319c8baf977600baa5bc5f64d704cca4f94
Si Hyeong Lee <super2451894@naver.com>
2017-08-30 15:45:57 +0900

    If there is no Function.js

---
+++ JavaScript/6. Function/If there is no function.js
@@ -0,0 +1,29 @@
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  i += 1;
+}
+
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  i += 1;
+}
+
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  i += 1;
+}
+
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  i += 1;
+}
+
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  i += 1;
+}
