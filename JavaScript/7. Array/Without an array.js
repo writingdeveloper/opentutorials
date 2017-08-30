4e08e9a044635e1ff6592ae50ddcacecaed0653a
Si Hyeong Lee <super2451894@naver.com>
2017-08-30 16:09:19 +0900

    Without Array.js

---
+++ JavaScript/7. Array/Without an array.js
@@ -0,0 +1,15 @@
+function get_member1() {
+  return 'sangumee';
+}
+console.log(get_member1());
+
+function get_member2() {
+  return 'gumgum';
+}
+console.log(get_member2());
+
+
+function get_member3() {
+  return 'bobbob'
+}
+console.log(get_member3());
