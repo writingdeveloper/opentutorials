86f69b9079afb08fa2de7324f58edae16f08fed6
Si Hyeong Lee <super2451894@naver.com>
2017-08-31 20:53:11 +0900

    Global and Local Variables.js

---
+++ JavaScript/10. Effective range/Global and local variables.js
@@ -0,0 +1,21 @@
+//Global Variable
+
+var vscope = 'global';
+
+function fscope() {
+  console.log(vscope)
+};
+
+fscope();
+
+// Local Variable
+
+var vscope = 'global';
+
+function fscope() {
+  var vscope = 'local';
+  console.log('함수안 ' + vscope);
+}
+
+fscope();
+console.log('함수밖' + vscope);
