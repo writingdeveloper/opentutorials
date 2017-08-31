58e1e29f52652199c0b7b0cde80f48185127cd96
Si Hyeong Lee <super2451894@naver.com>
2017-08-31 14:27:02 +0900

    Using loops in Object

---
+++ JavaScript/8. Object/Using loops in objects.js
@@ -0,0 +1,18 @@
+var grades = {
+  'sangumee': 10,
+  'gumgum': 6,
+  'bobbob': 80
+};
+
+for (key in grades) {
+  console.log('key : ' + key + ' value : ' + grades[key]);
+}
+
+/*
+
+key : sangumee value : 10
+key : gumgum value : 6
+key : bobbob value : 80
+[Finished in 0.209s]
+
+*/
