b9159656cc72240203a2ea825957c1d4bd569df0
Si Hyeong Lee <super2451894@naver.com>
2017-08-31 14:47:08 +0900

    Module Load in Node.js

---
+++ JavaScript/9. Module/Module Load in Node.js/node.memo.js
@@ -0,0 +1,9 @@
+var circle = require('./node.circle.js');
+console.log('The area of a circle of radius 4 is ' + circle.area(4));
+
+/*
+
+F:\BitNami\wampstack-5.4.22-0\apache2\htdocs\javascript\module>node node.demo.js
+The area of a circle of radius 4 is 50.26548245743669
+
+*/
