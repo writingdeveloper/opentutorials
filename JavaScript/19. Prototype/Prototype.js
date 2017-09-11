23df7ab0240b74d95833dc196113879d0e522836
Si Hyeong Lee <super2451894@naver.com>
2017-09-11 17:04:17 +0900

    Prototype.js

---
+++ JavaScript/19. Prototype/Prototype.js
@@ -0,0 +1,11 @@
+function Ultra() {}
+Ultra.prototype.ultraProp = true;
+
+function Super() {}
+Super.prototype = new Ultra();
+
+function Sub() {}
+Sub.prototype = new Super();
+
+var o = new Sub();
+console.log(o.ultraProp);
