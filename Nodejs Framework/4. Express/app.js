7e91b66801c4cba1294b2a97af16e2229d4954bc
Si Hyeong Lee <super2451894@naver.com>
2017-09-14 21:33:30 +0900

    express

--- Nodejs Framework/4. Express/app.js
+++ Nodejs Framework/4. Express/app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
   res.send("Hello Home Page");
 });
 
-app.listen(3000, function() {
-  console.log('Connected 3000 Port!');
+app.get('/login', function(req,res){
+  res.send('<h1>Login Please</h1>');
+});
+
+app.listen(3001, function() {
+  console.log('Connected 3001 Port!');
 });
