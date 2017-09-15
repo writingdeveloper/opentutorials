c10a360aa2a2da97dde3b9c089e5c0d4a52ac0ad
Si Hyeong Lee <super2451894@naver.com>
2017-09-15 16:49:08 +0900

    Jade

--- Nodejs Framework/6. Express - Jade/app.js
+++ Nodejs Framework/6. Express - Jade/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+app.locals.pretty=true;
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname,'public')));
