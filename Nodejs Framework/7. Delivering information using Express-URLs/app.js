c1d1ea1f003de16710b1902216c485991b64867a
Si Hyeong Lee <super2451894@naver.com>
2017-09-28 11:24:05 +0900

    Fix

--- Nodejs Framework/7. Delivering information using Express-URLs/app.js
+++ Nodejs Framework/7. Delivering information using Express-URLs/app.js
@@ -7,8 +7,23 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/topic',function(req, res){
-  res.send(req.query.id);
+app.get('/topic/:id',function(req, res){
+  var topics=[
+    'Javascript is...',
+    'Nodejs is...',
+    'Express is...'
+  ];
+  var output=`
+  <a href="/topic?id=0">JavaScript</a><br>
+  <a href="/topic?id=1">Nodejs</a><br>
+  <a href="/topic?id=2">Express</a><br><br>
+  ${topics[req.params.id]}
+  `
+  res.send(output);
+});
+
+app.get('/topic/:id/:mode',function(req,res){
+  res.send(req.params.id+','+req.params.mode);
 });
 
 app.get('/template',function(req,res){
