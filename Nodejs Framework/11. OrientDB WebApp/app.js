fc54dbcb52d6a0b14907c08e7d55cd961eb0f180
Si Hyeong Lee <super2451894@naver.com>
2017-10-12 17:49:48 +0900

    OrientDB CRUD APP Finish

--- Nodejs Framework/11. OrientDB WebApp/app.js
+++ Nodejs Framework/11. OrientDB WebApp/app.js
@@ -46,17 +46,79 @@ app.post('/upload', upload.single('userfile'), function(req, res) {
 });
 
 
-app.get('/topic/new', function(req, res) {
-  fs.readdir('data', function(err, files) {
-    if (err) {
-      console.log(err);
-      res.status(500).send('Internal Server Error');
+app.get('/topic/add', function(req, res) {
+  var sql = 'SELECT FROM topic';
+  db.query(sql).then(function(topics){
+    res.render('add', {topics: topics});
+  });
+});
+
+app.post('/topic/add', function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  var author=req.body.author;
+  var sql='INSERT INTO topic (title, description, author) VALUES(:title, :desc, :author)';
+  db.query(sql,{
+    params:{
+      title:title,
+      desc:description,
+      author:author
     }
-    res.render('new', {
-      topics: files
+  }).then(function(results){
+    res.redirect('/topic/'+encodeURIComponent(results[0]['@rid']));
+  });
+});
+
+app.get('/topic/:id/edit', function(req, res) {
+  var sql = 'SELECT FROM topic';
+  var id=req.params.id;
+  db.query(sql).then(function(topics){
+    var sql = 'SELECT FROM topic WHERE @rid=:rid';
+    db.query(sql, {params:{rid:id}}).then(function(topic){
+      res.render('edit', {topics:topics, topic:topic[0]});
     });
   });
 });
+app.post('/topic/:id/edit', function(req, res){
+   var sql = 'UPDATE topic SET title=:t, description=:d, author=:a WHERE @rid=:rid';
+   var id = req.params.id;
+   var title = req.body.title;
+   var desc = req.body.description;
+   var author = req.body.author;
+   db.query(sql, {
+     params:{
+       t:title,
+       d:desc,
+       a:author,
+      rid:id
+     }
+   }).then(function(topics){
+     res.redirect('/topic/'+encodeURIComponent(id));
+   });
+ });
+
+ app.get('/topic/:id/delete', function(req, res) {
+   var sql = 'SELECT FROM topic';
+   var id=req.params.id;
+   db.query(sql).then(function(topics){
+     var sql = 'SELECT FROM topic WHERE @rid=:rid';
+     db.query(sql, {params:{rid:id}}).then(function(topic){
+       res.render('delete', {topics:topics, topic:topic[0]});
+     });
+   });
+ });
+
+ app.post('/topic/:id/delete', function(req, res){
+    var sql = 'DELETE FROM topic WHERE @rid=:rid';
+    var id = req.params.id;
+    db.query(sql, {
+      params:{
+       rid:id
+      }
+    }).then(function(topics){
+      res.redirect('/topic/');
+    });
+  });
 
 app.get(['/topic', '/topic/:id'], function(req, res){
    var sql = 'SELECT FROM topic';
@@ -126,23 +188,10 @@ app.get('/topic/:id', function(req, res) {
         description: data
       });
     });
-
-
   })
-
-
 });
 
-app.post('/topic', function(req, res) {
-  var title = req.body.title;
-  var description = req.body.description;
-  fs.writeFile('data/' + title, description, function(err) {
-    if (err) { //If Error Exists
-      res.status(500).send('Internal Server Error');
-    }
-    res.redirect('/topic/' + title);
-  });
-});
+
 
 app.listen(3000, function() {
   console.log('Connected 3000 Port!!');
