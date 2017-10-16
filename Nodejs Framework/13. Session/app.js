10d31ad453a4acaf49a6c02e8c64b1de433c8419
Si Hyeong Lee <super2451894@naver.com>
2017-10-16 23:55:18 +0900

    Login Form

--- Nodejs Framework/13. Session/app.js
+++ Nodejs Framework/13. Session/app.js
@@ -19,10 +19,15 @@ app.get('/count', function(req, res) {
 
 app.get('/auth/login', function(req,res){
   var output=`
-  <form>
+  <form action="/auth/login" method="post">
     <p>
       <input type="text" name="username" placeholder="username">
+    </p>
+    <p>
       <input type="password" name="password" placeholder="password">
+    </p>
+    <p>
+    <input type="submit">
     </p>
   </form>
   `
