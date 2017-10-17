9cbf52bc09dc14a90ea7e01266e4fe592ef438c9
sangumee <super2451894@naver.com>
2017-10-17 18:33:32 +0900

    SHA256
    SHA256

--- Nodejs Framework/13. Session/app.js
+++ Nodejs Framework/13. Session/app.js
@@ -1,72 +1,66 @@
 var express = require('express');
 var session = require('express-session');
-var bodyParser = require('body-parser');
 var OrientoStore = require('connect-oriento')(session);
+var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-
 app.use(session({
-  secret: '2983718947@ROFKAMVVAAKDIWY',
+  secret: '1234DSFs@adf1234!@#$asd',
   resave: false,
   saveUninitialized: true,
   store: new OrientoStore({
     server: 'host=localhost&port=2424&username=root&password=password123&db=o2'
   })
 }));
-
 app.get('/count', function(req, res) {
   if (req.session.count) {
     req.session.count++;
   } else {
     req.session.count = 1;
   }
-  res.send('Count : ' + req.session.count);
+  res.send('count : ' + req.session.count);
 });
-
 app.get('/auth/logout', function(req, res) {
   delete req.session.displayName;
   req.session.save(function() {
     res.redirect('/welcome');
   });
 });
-
 app.get('/welcome', function(req, res) {
   if (req.session.displayName) {
     res.send(`
-      <h1>hello, ${req.session.displayName}</h1>
-      <a href="/auth/logout">Logout</a>
-      `);
+      <h1>Hello, ${req.session.displayName}</h1>
+      <a href="/auth/logout">logout</a>
+    `);
   } else {
     res.send(`
       <h1>Welcome</h1>
       <a href="/auth/login">Login</a>
-      `);
+    `);
   }
 });
-
 app.post('/auth/login', function(req, res) {
   var user = {
-    username: 'sangumee',
-    password: '1111',
-    displayName: 'Sangumee'
+    username: 'egoing',
+    password: '111',
+    displayName: 'Egoing'
   };
   var uname = req.body.username;
   var pwd = req.body.password;
   if (uname === user.username && pwd === user.password) {
     req.session.displayName = user.displayName;
     req.session.save(function() {
-    res.redirect('/welcome');
-  });
+      res.redirect('/welcome');
+    });
   } else {
-    res.send('Login Failed! <a href="/auth/login">Login</a>');
+    res.send('Who are you? <a href="/auth/login">login</a>');
   }
 });
-
 app.get('/auth/login', function(req, res) {
   var output = `
-  <h1>Login Page</h1>
+  <h1>Login</h1>
   <form action="/auth/login" method="post">
     <p>
       <input type="text" name="username" placeholder="username">
@@ -75,14 +69,12 @@ app.get('/auth/login', function(req, res) {
       <input type="password" name="password" placeholder="password">
     </p>
     <p>
-    <input type="submit">
+      <input type="submit">
     </p>
   </form>
-  `
-
+  `;
   res.send(output);
 });
-
-app.listen(3000, function() {
-  console.log('Connected 3000 Port!');
+app.listen(3003, function() {
+  console.log('Connected 3003 port!!!');
 });
