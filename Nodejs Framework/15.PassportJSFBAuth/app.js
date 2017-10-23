b546a51451f8852ad4db7af3986e8bebfdeffa5b
sangumee <super2451894@naver.com>
2017-10-23 17:27:00 +0900

    Facebook Auth

--- Nodejs Framework/15.PassportJSFBAuth/app.js
+++ Nodejs Framework/15.PassportJSFBAuth/app.js
@@ -3,9 +3,9 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var bodyParser = require('body-parser');
 var bkfd2Password = require("pbkdf2-password");
-var passport = require('passport')
+var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var FaceFacebookStrategy = require('passport-facebook').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var hasher = bkfd2Password();
 
 var app = express();
@@ -16,12 +16,9 @@ app.use(session({
   secret: '1234DSFs@adf1234!@#$asd',
   resave: false,
   saveUninitialized: true,
-  store: new FileStore()
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.get('/count', function(req, res) {
   if (req.session.count) {
     req.session.count++;
@@ -31,13 +28,15 @@ app.get('/count', function(req, res) {
   res.send('count : ' + req.session.count);
 });
 app.get('/auth/logout', function(req, res) {
-  delete req.session.displayName;
-  res.redirect('/welcome');
+  req.logout();
+  req.session.save(function() {
+    res.redirect('/welcome');
+  });
 });
 app.get('/welcome', function(req, res) {
-  if (req.session.displayName) {
+  if (req.user && req.user.displayName) {
     res.send(`
-      <h1>Hello, ${req.session.displayName}</h1>
+      <h1>Hello, ${req.user.displayName}</h1>
       <a href="/auth/logout">logout</a>
     `);
   } else {
@@ -50,31 +49,31 @@ app.get('/welcome', function(req, res) {
     `);
   }
 });
-
 passport.serializeUser(function(user, done) {
   console.log('serializeUser', user);
   done(null, user.authId);
 });
 passport.deserializeUser(function(id, done) {
   console.log('deserializeUser', id);
-  for(var i=0; i<users.length; i++){
+  for (var i = 0; i < users.length; i++) {
     var user = users[i];
-    if(user.authId === id){
+    if (user.authId === id) {
       return done(null, user);
     }
   }
-  done('There is no user.');
 });
-
 passport.use(new LocalStrategy(
-  function(username, password, done){
+  function(username, password, done) {
     var uname = username;
     var pwd = password;
-    for(var i=0; i<users.length; i++){
+    for (var i = 0; i < users.length; i++) {
       var user = users[i];
-      if(uname === user.username) {
-        return hasher({password:pwd, salt:user.salt}, function(err, pass, salt, hash){
-          if(hash === user.password){
+      if (uname === user.username) {
+        return hasher({
+          password: pwd,
+          salt: user.salt
+        }, function(err, pass, salt, hash) {
+          if (hash === user.password) {
             console.log('LocalStrategy', user);
             done(null, user);
           } else {
@@ -86,34 +85,78 @@ passport.use(new LocalStrategy(
     done(null, false);
   }
 ));
-
-app.post('/auth/login', passport.authenticate('local', {
-  successRedirect: '/welcome',
-  failureRedirect: '/auth/login',
-  failureFlash: false
-}));
-
-
+passport.use(new FacebookStrategy({
+    clientID: '152673615470530',
+    clientSecret: '9fd2e54f4292aa8913b996a33bdce734',
+    callbackURL: "/auth/facebook/callback",
+    profileFields:['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified', 'displayName']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    var authId = 'facebook:' + profile.id;
+    for (var i = 0; i < users.length; i++) {
+      var user = users[i];
+      if (user.authId === authId) {
+        return done(null, user);
+      }
+    }
+    var newuser = {
+      'authId': authId,
+      'displayName': profile.displayName,
+      'email':profile.emails[0].value
+    };
+    users.push(newuser);
+    done(null, newuser);
+  }
+));
+app.post(
+  '/auth/login',
+  passport.authenticate(
+    'local', {
+      successRedirect: '/welcome',
+      failureRedirect: '/auth/login',
+      failureFlash: false
+    }
+  )
+);
+app.get(
+  '/auth/facebook',
+  passport.authenticate(
+    'facebook'
+  )
+);
+app.get(
+  '/auth/facebook/callback',
+  passport.authenticate(
+    'facebook', {
+      successRedirect: '/welcome',
+      failureRedirect: '/auth/login'
+    }
+  )
+);
 var users = [{
-  username: 'egoing',
+  authId: 'local:sangumee',
+  username: 'sangumee',
   password: 'mTi+/qIi9s5ZFRPDxJLY8yAhlLnWTgYZNXfXlQ32e1u/hZePhlq41NkRfffEV+T92TGTlfxEitFZ98QhzofzFHLneWMWiEekxHD1qMrTH1CWY01NbngaAfgfveJPRivhLxLD1iJajwGmYAXhr69VrN2CWkVD+aS1wKbZd94bcaE=',
   salt: 'O0iC9xqMBUVl3BdO50+JWkpvVcA5g2VNaYTR5Hc45g+/iXy4PzcCI7GJN5h5r3aLxIhgMN8HSh0DhyqwAp8lLw==',
-  displayName: 'Egoing'
+  displayName: 'sangumee'
 }];
 app.post('/auth/register', function(req, res) {
   hasher({
     password: req.body.password
   }, function(err, pass, salt, hash) {
     var user = {
+      authId: 'local:' + req.body.username,
       username: req.body.username,
       password: hash,
       salt: salt,
       displayName: req.body.displayName
     };
     users.push(user);
-    req.session.displayName = req.body.displayName;
-    req.session.save(function() {
-      res.redirect('/welcome');
+    req.login(user, function(err) {
+      req.session.save(function() {
+        res.redirect('/welcome');
+      });
     });
   });
 });
@@ -151,7 +194,7 @@ app.get('/auth/login', function(req, res) {
       <input type="submit">
     </p>
   </form>
-  <a href="/auth/facebook">Facebook</a>
+  <a href="/auth/facebook">facebook</a>
   `;
   res.send(output);
 });
