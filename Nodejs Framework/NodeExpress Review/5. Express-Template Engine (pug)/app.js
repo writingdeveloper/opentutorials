a01b80fe443c2a0389a6a542a5da0663a5087b60
sangumee <super2451894@naver.com>
2017-11-28 16:21:37 +0900

    Express-Template Engine Pug
    Express-Template Engine Pug

---
+++ Nodejs Framework/NodeExpress Review/5. Express-Template Engine (pug)/app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.locals.pretty = true;
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/template', function(req, res) {
+  res.render('temp', {
+    time: Date(),
+    title: 'Pug'
+  });
+})
+app.get('/', function(req, res) {
+  res.send('Hello home page');
+});
+app.get('/dynamic', function(req, res) {
+  var lis = '';
+  for (var i = 0; i < 5; i++) {
+    lis = lis + '<li>coding</li>';
+  }
+  var time = Date();
+  var output = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+        Hello, Dynamic!
+        <ul>
+          ${lis}
+        </ul>
+        ${time}
+    </body>
+  </html>`;
+  res.send(output);
+});
+app.get('/route', function(req, res) {
+  res.send('Hello Router, <img src="/route.png">')
+})
+app.get('/login', function(req, res) {
+  res.send('<h1>Login please</h1>');
+});
+app.listen(3000, function() {
+  console.log('Conneted 3000 port!');
+});
