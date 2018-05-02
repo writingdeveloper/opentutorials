43171a04acdf04c21fb28d2b3576fa6c6e670616
sangumee <super2451894@gmail.com>
2018-05-02 13:50:15 +0900

    PHP Study
    PHP Study

---
+++ PHP/4. Compare/compare4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "10>=20 : ";
+    var_dump(10>=20);
+    echo '<br />';
+    echo "10>=1 : ";
+    var_dump(10>=1);
+    echo '<br />';
+    echo "10>=10 : ";
+    var_dump(10>=10);
+    echo '<br />';
+     ?>
+  </body>
+</html>
---
+++ PHP/4. Compare/compare5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "1 == '1' : ";
+    var_dump(1=='1');
+    echo '<br />';
+    echo "1 === '1' : ";
+    var_dump(1==='1');
+    echo '<br />';
+
+     ?>
+  </body>
+</html>
