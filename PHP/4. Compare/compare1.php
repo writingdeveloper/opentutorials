24c895564af11619a523e1605495d9e7b2baa190
sangumee <super2451894@gmail.com>
2018-05-02 13:11:52 +0900

    PHP Study
    PHP Study

---
+++ PHP/4. Compare/compare1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "1==2 : ";
+    var_dump(1==2);
+    echo '<br />';
+    echo "1==1 : ";
+    var_dump(1==1);
+    echo '<br />';
+    echo '"one"=="two" :';
+    var_dump("one"=="two");
+    echo '<br />';
+    echo '"one"=="one" : ';
+    var_dump("one"=="one");
+    echo '<br />';
+     ?>
+  </body>
+</html>
