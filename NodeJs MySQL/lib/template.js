1bfa04eeb3133c3dfca4d1c97752fd3b2f542e66
sangumee <super2451894@gmail.com>
2018-07-06 14:45:02 +0900

    Implement author list view
    Implement author list view

--- NodeJs MySQL/lib/template.js
+++ NodeJs MySQL/lib/template.js
@@ -9,6 +9,7 @@ module.exports = {
     </head>
     <body>
       <h1><a href="/">WEB</a></h1>
+      <a href="/author">author</a>
       ${list}
       ${control}
       ${body}
@@ -30,9 +31,9 @@ module.exports = {
     var tag = '';
     var i = 0;
     while (i < authors.length) {
-      var selected ='';
-      if(authors[i].id === author_id){
-        selected=' selected';
+      var selected = '';
+      if (authors[i].id === author_id) {
+        selected = ' selected';
       }
       tag += tag + `<option value="${authors[i].id}"${selected}>${authors[i].name}</option>`;
       i++;
@@ -42,5 +43,22 @@ module.exports = {
                 ${tag}
               </select>
     `
+  },
+  authorTable: function (authors) {
+    var tag = '<table>';
+    var i = 0;
+    while (i < authors.length) {
+      tag += `
+            <tr>
+                <td>${authors[i].name}</td>
+                <td>${authors[i].profile}</td>
+                <td>update</td>
+                <td>delete</td>
+            </tr>
+                `
+      i++;
+    }
+    tag += '</table>';
+    return tag;
   }
 }
