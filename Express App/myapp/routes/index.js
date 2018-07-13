bd6358762d27b7a03a2a71015eb4dfe7c8cf6067
sangumee <super2451894@gmail.com>
2018-07-13 14:03:23 +0900

    ZzapCord MySQL
    ZzapCord MySQL

--- Express App/myapp/routes/index.js
+++ Express App/myapp/routes/index.js
@@ -1,33 +1,16 @@
 var express = require('express');
-var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  var service_key = 'zpIsbBaqMwp8mr7Qcc%2BJlzlBJb41v6vdpZaICOpCkn%2BtYeumWZUQxAHjGB7XEYRRcroVZnEOsSTHBCKVGUJmsA%3D%3D';
-
-  var url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getUnityAirEnvrnIdexSnstiveAboveMsrstnList';
-  var queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + service_key; // Service Key
-
-  request({
-    url: url + queryParams,
-    method: 'GET'
-  }, function (error, response, body) {
-    console.log('Status', response.statusCode);
-    console.log('Headers', JSON.stringify(response.headers));
-    console.log('Reponse received', body);
-
-    var html = res.render('index' , { 
-      title : 'express',
-      addrs: body 
-    });
-    console.log(html);
-  });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req,res, next){
-  res.render('login', {title: 'Login'});;
+/* GET login page. */
+router.get('/login', function (req, res, next) {
+  res.render('login', {
+    title: 'Login'
+  });
 });
 
 module.exports = router;
